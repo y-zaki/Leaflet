ee188f696731e1bd237f60942ffe1eb2e6ae30e5
y-zaki <sg-1.048596@moegi.waseda.jp>
2019-06-24 12:46:01 +0900

    second commit

--- app/assets/map_display.js
+++ app/assets/map_display.js
@@ -1,3 +1,10 @@
+//******json data*****
+var data = [
+  { name : "早稲田大学", position : { lat : 35.7091, lng : 139.7222 } },
+  { name : "高田馬場駅", position : { lat : 35.7128, lng : 139.7036 } }
+];
+//******json data*****
+
 var map = L.map('map');
 var tileLayer = L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
 {attribution:'© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
@@ -14,11 +21,19 @@ function onLocationFound(e) {
     var Departure_lat = e.latlng.lat;
     var Departure_lng = e.latlng.lng;
 
+    //目的地の入力
+    goal = window.prompt("目的地を入力してください！", "");
+
+    //一致するものを探す
+    var matchData = data.filter(function(item, index){
+      if (item.name == goal) return true;
+    });
+
     //経路探索
     L.Routing.control({
           waypoints: [
               L.latLng(Departure_lat, Departure_lng), //現在地
-              L.latLng(35.709165, 139.722222) //早稲田キャンパス
+              L.latLng(matchData[0].position.lat, matchData[0].position.lng) //目的地
           ],
           routeWhileDragging: true
       }).addTo(map);
